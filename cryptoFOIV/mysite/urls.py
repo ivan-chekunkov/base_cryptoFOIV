40168b61451c30e34c21cefcb1e8438004c0e5e9
Ivan Chekunkov <chekunkov.ivan@yandex.ru>
2025-04-14 20:00:17 +0300

    up urls

--- cryptoFOIV/mysite/urls.py
+++ cryptoFOIV/mysite/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("crypto/", include("crypto.urls")),
+    path("", include("crypto.urls")),
     path("admin/", admin.site.urls),
 ]
